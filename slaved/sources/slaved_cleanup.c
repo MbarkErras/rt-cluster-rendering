c0037673f0fa70f171ec4af765da7cbbc6d2c6c8
M'bark Erras <mbarekerras@gmail.com>
2020-03-01 23:54:15 +0100

    norming slaved

---
+++ slaved/sources/slaved_cleanup.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   slaved_cleanup.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merras <merras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/01 20:52:38 by merras            #+#    #+#             */
+/*   Updated: 2020/03/01 21:45:42 by merras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "slaved.h"
+
+void	slaved_cleanup(t_slaved *slaved)
+{
+	kill(slaved->program.pid, 3);
+	close(slaved->program.r_stdout);
+	close(slaved->program.w_stdin);
+	list_delete(&slaved->packets_queue, destroy_packet);
+}
---
+++ slaved/sources/slaved_routines.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   slaved_routines.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merras <merras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/01 20:06:36 by merras            #+#    #+#             */
+/*   Updated: 2020/03/01 23:48:19 by merras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "slaved.h"
+
+static void	handle_packet(void *slaved)
+{
+	t_packet		response;
+	t_dstruct_node	*request;
+
+	request = CAST(slaved, t_slaved *)->packets_queue.head;
+	queue_dequeue(&CAST(slaved, t_slaved *)->packets_queue, NULL);
+	pthread_mutex_unlock(&CAST(slaved, t_slaved *)->packets_queue_mx);
+	response = requests_dispatcher(request->content, slaved);
+	destroy_packet(request->content);
+	free(request);
+	if (write_packet(CAST(slaved, t_slaved *)->sockets[1], response) ||
+		response.type == TYPE_T_RESPONSE_FAILURE ||
+		CAST(request->content, t_packet *)->type == TYPE_T_REQUEST_COMPUTATION)
+	{
+		pthread_cancel(CAST(slaved, t_slaved *)->tid[0]);
+		pthread_exit(NULL);
+	}
+}
+
+void		*handle_packets(void *slaved)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&CAST(slaved, t_slaved *)->packets_queue_mx);
+		if (!CAST(slaved, t_slaved *)->packets_queue.size)
+		{
+			pthread_mutex_unlock(&CAST(slaved, t_slaved *)->packets_queue_mx);
+			continue;
+		}
+		else
+			handle_packet(slaved);
+	}
+}
+
+void		*receive_packets(void *slaved)
+{
+	t_packet request;
+
+	LOG_INFO("running receive_packets() routine.");
+	while (1)
+	{
+		LOG_INFO("reading a request.");
+		if (read_packet(CAST(slaved, t_slaved *)->sockets[1], &request))
+		{
+			LOG_ERROR("exiting receive_packets(), read_packet() failed()");
+			pthread_exit(NULL);
+		}
+		pthread_mutex_lock(&CAST(slaved, t_slaved *)->packets_queue_mx);
+		LOG_INFO("enquing a request.");
+		queue_enqueue(&CAST(slaved, t_slaved *)->packets_queue,
+			t_dstruct_create_node(create_packet(request, NULL),
+				sizeof(t_packet)));
+		pthread_mutex_unlock(&CAST(slaved, t_slaved *)->packets_queue_mx);
+		if (request.type == TYPE_T_REQUEST_COMPUTATION)
+		{
+			LOG_WARN("receive_packets: enqueing a COMPUTATION, I am done.");
+			pthread_exit(NULL);
+		}
+	}
+}
